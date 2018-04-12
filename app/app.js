f8428597d9bb22073534d5d9e62ef79ce7518a99
sandeepdj <sandeepdj1988@gmail.com>
2018-04-12 14:52:33 +0530

    dashboard

--- app/app.js
+++ app/app.js
@@ -62,7 +62,52 @@ app.directive("collapseNavAccordion", ["$rootScope", function($rs) {
 app.controller("headerController", function($scope) {
 
 })
+app.directive("sparkline", [function() {
+    return {
+        restrict: "EA",
+        link: function(scope, el, attrs) {
+            var model = attrs.values || el.text();
+            var opts = {};
 
+            if (attrs.opts) angular.extend(opts, angular.fromJson(attrs.opts));
+            else {
+                angular.extend(opts, attrs);
+            }
+
+            // The following options have to be converted to array from string
+            // Type 		Options
+            // ====			========
+            // Bar 			stackedBarColor
+            // Bullet		rangeColors
+            // Pie 			sliceColors
+            // NOte: when sepcifying multiple colors for above attributes, don't put spaces between them
+            // Eg: [#4CAF50,#38B4EE,#eee]  // right
+            // 	[#4CAF50, #38B4EE, #eee]	// wrong
+
+            // for bar
+            if (attrs.stackedBarColor) {
+                opts.stackedBarColor = attrs.stackedBarColor.replace("[", "").replace("]", "").split(",");
+            }
+            // for bullet
+            if (attrs.rangeColors) {
+                opts.rangeColors = attrs.rangeColors.replace("[", "").replace("]", "").split(",");
+            }
+            // for pie
+            if (attrs.sliceColors) {
+                opts.sliceColors = attrs.sliceColors.replace("[", "").replace("]", "").split(",");
+
+            }
+
+
+
+            if (angular.isString(model))
+                model = JSON.parse("[" + model + "]");
+
+            el.sparkline(model, opts);
+
+        }
+    }
+}])
 
 
 app.controller("AppCtrl", ["$rootScope", "$scope", "$timeout", function($rs, $scope, $timeout) {
@@ -289,4 +334,69 @@ app.controller("TodoCtrl", ["$scope", "todoStorage", "$filter", function($s, sto
             todo.completed = !completed
         }), $s.remainingCount = completed ? todos.length : 0, store.put(todos)
     }
-}])
+}]);
+app.factory('c3Factory', ['$q', '$timeout', function($q, $timeout) {
+    var defer = $q.defer();
+    var chart = {};
+    var allCharts = {};
+    var decorateChart = function(chart) {};
+
+    chart.get = function(id) {
+        var chart;
+        return $timeout(function() {
+            //time out to wait for the chart to be compiled
+        }, 100).then(function() {
+            chart = allCharts[id];
+            return chart;
+        });
+    };
+
+    chart.getAll = function() {
+        return $timeout(function() {
+            return allCharts;
+        }, 100);
+    };
+
+    chart.register = function(id, chart) {
+        decorateChart(chart);
+        allCharts[id] = chart;
+    };
+
+    return chart;
+}]);
+app.directive('c3Chart', ['c3Factory', function(c3Factory) {
+
+    //color patterns for chart coloring
+    var patterns = {
+        light: ['#1f77b4', '#aec7e8', '#ff7f0e', '#ffbb78', '#2ca02c', '#98df8a', '#d62728', '#ff9896'],
+        dark: ['#9467bd', '#c5b0d5', '#8c564b', '#c49c94', '#e377c2', '#f7b6d2', '#7f7f7f', '#c7c7c7'],
+        material: ['#e51c23', '#673ab7', '#5677fc', '#03a9f4', '#00bcd4', '#259b24', '#ffeb3b', '#ff9800']
+    };
+
+    return {
+        restrict: 'EAC',
+        scope: {
+            config: '='
+        },
+        template: '<div></div>',
+        replace: true,
+        link: function(scope, element, attrs) {
+            //available option to show gridlines for chart
+            //assign a type of line if undefined
+            if (!scope.config.type) scope.config.type = 'line';
+
+            //generate c3 chart data
+            var chartData = scope.config;
+            chartData.bindto = '#' + attrs.id;
+
+            //Generating the chart
+            var chart = c3.generate(chartData);
+            c3Factory.register(attrs.id, chart);
+            // bind resize event
+
+            scope.$on("c3.resize", function(e, data) {
+                chart.resize();
+            });
+        }
+    };
+}]);
